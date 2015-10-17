f09aa42fa77a5328a924cb56d62be508debd3920
Dmitriy Zimy Yakovlev <zimy@yandex.ru>
2015-10-17 19:27:05 +0300

    Added fallback cached version

--- src/main/java/me/zimy/parker/controllers/WebInterface.java
+++ src/main/java/me/zimy/parker/controllers/WebInterface.java
@@ -6,11 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.ExecutionException;
 
 /**
  * Main and only HTTP controller
@@ -21,9 +21,22 @@ import java.util.concurrent.ExecutionException;
 @Controller
 @RequestMapping("/")
 public class WebInterface {
-
+    @Autowired
+    Thermos thermos;
     @RequestMapping("/")
-    public String getProbabilityResults(Model model) throws InterruptedException, ExecutionException {
+    public String getProbabilityResults(
+            @RequestParam(defaultValue = "false", required = false) Boolean cache,
+            @RequestParam(defaultValue = "false", required = false) Boolean mock,
+            Model model) {
+        List<ThermoEvent> all = thermos.findAll();
+        model.addAttribute("secondResult", all);
+        model.addAttribute("lastValue", all.get(all.size() - 1));
+        model.addAttribute("cacheable", false);
+        return "answers";
+    }
+
+    @RequestMapping("/persistent")
+    public String getMockedAndCached(Model model){
         Random random = new Random();
         List<Integer> parts = new ArrayList<>();
         for (int i = 0; i < 13; i++) {
@@ -31,6 +44,7 @@ public class WebInterface {
         }
         model.addAttribute("secondResult", parts);
         model.addAttribute("lastValue", parts.get(parts.size() - 1));
+        model.addAttribute("cacheable", true);
         return "answers";
     }
 }
